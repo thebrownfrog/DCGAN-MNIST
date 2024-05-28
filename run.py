a7ba7949feec557a704b5151067233090e025941
thebrownfrog <158177659+thebrownfrog@users.noreply.github.com>
2024-05-28 20:26:45 +0200

    Removed unused class "ResidualBlock" in run.py

--- run.py
+++ run.py
@@ -36,30 +36,6 @@ test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=Fa
 def noise(size):
     return torch.randn(size, latent_size[0], device="cuda")*13.5, torch.randint(0,latent_size[1],(size,),device="cuda").long()
 
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, activation = F.leaky_relu):
-        super(ResidualBlock, self).__init__()
-
-        self.activation = activation
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        
-        self.shortcut = nn.Sequential()
-        if in_channels != out_channels:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, bias=False),
-                nn.BatchNorm2d(out_channels)
-            )
-
-    def forward(self, x):
-        out = self.activation(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = self.activation(out)
-        return out
-
 class GAN:
     class Generator(nn.Module):
         def __init__(self):
