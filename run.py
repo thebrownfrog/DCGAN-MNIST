230251365847d08e6284d96c89db1615ccb33146
thebrownfrog <158177659+thebrownfrog@users.noreply.github.com>
2024-05-28 19:57:11 +0200

    removed unnecessary imports

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-import sys
-import os
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import torch
 import torchvision
 from torchvision import datasets, transforms
